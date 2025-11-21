4e15eac8a0d9545d5f3363285bbe84237a464c75
eliagarc <elias16199@gmail.com>
2025-11-21 03:24:26 +0100

    Test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center py-4 px-6 border-b border-gray-800 sticky top-0 bg-black backdrop-blur z-50">
-      <Link href="/" className="text-2xl font-bold text-pink-300">Portfolio Dreslser</Link>
+      <Link href="/" className="text-2xl font-bold text-pink-300">Portfolio Dresler</Link>
       <div className="space-x-6">
         <Link href="#inicio" className="hover:text-pink-300">Inicio</Link>
         <Link href="#about" className="hover:text-pink-300">Sobre mi</Link>
